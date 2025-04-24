dc225d83cd3473248375210f1f7e7968f5cf7194
aslv24 <aslinfomats@gmail.com>
2025-04-24 15:34:08 +0530

    add method added in the code

--- Sample.java
+++ Sample.java
@@ -1,6 +1,17 @@
 public class Sample{
+
+public static void add()
+{
+
+int a=10;
+
+int b=20;
+
+System.out.println(a+b);
+}
 	public static void main(String[] args)
 	{
 System.out.println("Hello World!!");
+add();
 	}
 }
