c6a7dfa7de2ba53bce995f9ed76310e0d3cf26e0
aslv24 <aslinfomats@gmail.com>
2025-04-25 16:44:52 +0530

    Sub Function added

--- Sample.java
+++ Sample.java
@@ -9,9 +9,18 @@ int b=20;
 
 System.out.println(a+b);
 }
+public static void sub()
+{
+int a=20;
+
+int b=10;
+
+System.out.println(a-b);
+}
 	public static void main(String[] args)
 	{
 System.out.println("Hello World!!");
 add();
+sub();
 	}
 }
